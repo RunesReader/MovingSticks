5a9b9171974612663333f7795b189fd03df76d63
RunesReader <1000000000g@gmail.com>
2015-11-23 13:33:20 +0200

    Initial commit.

---
+++ PhoneSpin/PhoneSpin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhoneSpin
+//
+//  Created by Igor Arsenkin on 23.11.15.
+//  Copyright © 2015 Igor Arsenkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
